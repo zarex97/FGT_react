3636935a778e24708676916656c1c3b718ce1d4e
zarex97 <zarexlibertad@gmail.com>
2024-10-23 05:12:36 -0300

    update: server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+console.log('WebSocket server is listening on port 8080');
+
 const clients = new Map();
 
 wss.on('connection', (ws) => {
