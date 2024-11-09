fe5df73027dd7b7aaa7a52b408a8661abf7561f0
zarex97 <131675618+zarex97@users.noreply.github.com>
2024-11-09 14:20:37 -0300

    feat: structurate servants, skills, servant classes

---
+++ src/game/servants/archer/index.js
@@ -0,0 +1,10 @@
+// src/game/servants/archer/index.js
+import { AnastasiaSkills, AnastasiaTemplate } from './Anastasia_unit';
+
+export const ArcherServants = {
+    Anastasia: {
+        template: AnastasiaTemplate,
+        skills: AnastasiaSkills
+    }
+    // Add other Archer servants here
+};
