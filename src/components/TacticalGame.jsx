1d6184daf406cdce67000db27865a7368f60509c
zarex97 <131675618+zarex97@users.noreply.github.com>
2024-11-11 17:40:02 -0300

    feat: added vision to anastasia

--- src/components/TacticalGame.jsx
+++ src/components/TacticalGame.jsx
@@ -58,6 +58,7 @@ const TacticalGame = ({ username, roomId }) => {
                         atk: 8, 
                         def: 5, 
                         movementRange: 5,
+                        visionRange: 4,
                         movementLeft: 5,
                         hasAttacked: false,
                         name: 'Anastasia',
@@ -838,6 +839,7 @@ const DetectionResults = ({ results }) => {
                         {result.wasDetected ? (
                             <span className="text-green-400 ml-2">Detected!</span>
                         ) : (
+
                             <span className="text-red-400 ml-2">Remained Hidden</span>
                         )}
                     </div>
